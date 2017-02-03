0a9f19a754885352b1d18f325564cb03c0e7b77f
Brandon McClinton <bmcclint@Brandons-Mac.mshome.net>
2017-02-03 11:37:26 -0600

    major bug fixed

--- src/media/converter.java
+++ src/media/converter.java
@@ -11,16 +11,12 @@ import com.mpatric.mp3agic.Mp3File;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
-import java.util.regex.Pattern;
 
 /**
  *
  * @author Brandon McClinton
  */
 public class converter {
-  public ArrayList<String> stuff = new ArrayList<>();
 
   public static void main(String[] args) {
     fileList(new File(args[0]));
@@ -44,32 +40,60 @@ public class converter {
       if (song.hasId3v2Tag()) {
         ID3v2 tag = song.getId3v2Tag();
         String artist = tag.getArtist();
-        System.out.println("Artist: " + artist.replaceAll("/", " "));
+        System.out.println("Artist: " + artist);
         String title = tag.getTitle();
-        System.out.println("Title: " + title.replaceAll("()*-+=/", " "));
+        System.out.println("Title: " + title);
         String albArt = tag.getAlbumArtist();
-        System.out.println("Album Artist: " + albArt.replaceAll("()*-+=/", " ") + "\n");
+        System.out.println("Album Artist: " + albArt + "\n");
+        replaceTag(file, artist, title, filePath);
         renameSong(file, artist, title, albArt);
+
+      } else if (!song.hasId3v2Tag()) {
+        System.out.println("Dig deeper! \n");
+      }
+      potsong.close();
+    } catch (IOException | BaseException e) {
+    }
+  }
+
+  private static void replaceTag(File file, String artist, String title, String filePath) {
+    try (FileInputStream newTags = new FileInputStream(file)) {
+      Mp3File song = new Mp3File(filePath);
+      ID3v2 tag = song.getId3v2Tag();
+      if (artist.contains("/") || title.contains("/")) {
+        String artistReplace = artist.replace("/", " ");
+        tag.setArtist(artistReplace);
+        System.out.println("New Artitst: " + artistReplace);
+        
+        String titleReplace = title.replace("/", " ");
+        tag.setTitle(titleReplace);
+        System.out.println("New Title: " + titleReplace + "\n");
+        
+        newTags.close();
+        renameSongNewTags(file, artistReplace, titleReplace);
       }
     } catch (IOException | BaseException e) {
+
     }
   }
 
   public static void renameSong(File file, String title, String artist, String albArt) {
     String path = "/Users/bmcclint/Desktop/Renamed_Tracks/";
-    try {
-      FileInputStream renamed = new FileInputStream(file);
+    try (FileInputStream renamed = new FileInputStream(file)) {
       file.renameTo(new File(path + title + " - " + artist + ".mp3"));
-//        System.out.println(file);
-      
+      System.out.println(file.getName());
+      renamed.close();
     } catch (IOException e) {
     }
   }
   
-  public static void stringTokenizer(String title, String artist, String albArt) {
-    StringTokenizer str = new StringTokenizer(artist, "()+-*/");
-    while (str.hasMoreTokens()) {
-      System.out.println(str.nextToken());
+  public static void renameSongNewTags(File file, String artistReplace, String titleReplace) {
+    String path = "/Users/bmcclint/Desktop/Renamed_Tracks/";
+    try (FileInputStream newTagsName = new FileInputStream(file)) {
+      file.renameTo(new File(path + artistReplace + " - " + titleReplace + ".mp3"));
+      System.out.println(file.getName());
+      newTagsName.close();
     }
-  }  
+    catch (IOException e) {}
+  }
 }
